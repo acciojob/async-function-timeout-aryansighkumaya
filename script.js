55163763870301c70c2687119d10d0e140543d03
aryansighkumaya <98814995+aryansighkumaya@users.noreply.github.com>
2024-12-20 00:25:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-//your JS code here. If required.
+        
+        
 
-const textInput=document.getElementById('text')
-const delayInput = document.getElementById('delay');
-const outputDiv=document.getElementById('output')
 const btn=document.getElementById('btn')
-
-   textInput.value = '';
-    delayInput.value = '';
-    outputDiv.innerText = '';
 
  function getData(text,delay){
 
@@ -27,15 +21,22 @@ const btn=document.getElementById('btn')
 
 
 async function fetchData(){
+const textInput=document.getElementById('text')
+const delayInput = document.getElementById('delay');
+const outputDiv=document.getElementById('output')
+
+  outputDiv.textContent = "";
+	
     event.preventDefault();
     try {
-        const text = textInput.value; // Get the text from input
-
+const text = textInput.value; // Get the text from input
 const delay=delayInput.value
-       
+        if (text && delay) {
+          const data = await getData(text,delay); // Wait for the promise to resolve
+        outputDiv.textContent = data; // Display the resolved value
+        }
 
-        const data = await getData(text,delay); // Wait for the promise to resolve
-        outputDiv.innerText = data; // Display the resolved value
+        
     } catch (error) {
         console.error("Error:", error);
     }
